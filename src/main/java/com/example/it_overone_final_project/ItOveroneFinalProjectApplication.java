06eb6835a6c0381ee998a2d449a5917d290e4cb8
Olga Lavrinova <lavrlavv@gmail.com>
2023-03-07 12:05:15 +0300

    1.1

---
+++ src/main/java/com/example/it_overone_final_project/ItOveroneFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.it_overone_final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItOveroneFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItOveroneFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/it_overone_final_project/ItOveroneFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.it_overone_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItOveroneFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
